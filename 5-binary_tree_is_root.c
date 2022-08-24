8c125ee8a21a3fa8f2667d71e7c4914123bc5d7a
Xmuluneh <mulabr673@gmail.com>
2022-08-24 12:08:59 +0300

    Check the given node is root node

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,11 @@
+# include "binary_trees.h"
+/**
+ * binary_tree_is_root -check  the node is root node
+ * @node: is a pointer to the node to check
+ * Return: 1 if the node is root node otherwise 0
+ */
+
+	int binary_tree_is_root(const binary_tree_t *node)
+	{
+		return ((!node || node->parent) ? 0 : 1); 
+	}
