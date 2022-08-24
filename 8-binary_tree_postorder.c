f971251a3e5079f07b1ffcbfcbcc1cfdee317402
Xmuluneh <mulabr673@gmail.com>
2022-08-24 13:22:18 +0300

    PostOrder traversing

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - If the tree is not empty,
+ * recursively traverse the left subtree, then the right subtree, then the root
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: A pointer to a function to call for each node.
+ * Return:void.
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+if (!tree || !func)
+	return;
+
+binary_tree_postorder(tree->left, func);
+binary_tree_postorder(tree->right, func);
+func(tree->n);
+}
