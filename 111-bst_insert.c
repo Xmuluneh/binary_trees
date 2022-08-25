c98f6ea6c6d80e2d32f04299271df59b16d0d1cf
Xmuluneh <mulabr673@gmail.com>
2022-08-25 15:13:38 +0300

    Insert node to BST

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,19 +1,19 @@
-#include "bitstring.h"
+#include "binary_trees.h"
+
 /**
- * bst_insert -If the tree is empty
- * create a new node and return it
+ * bst_insert -If the tree is empty,
+ * create a new node and return it.
  * Otherwise, find the correct place to insert
  * the new node and return it
  * @tree: a pointer to the root node of the BST to insert the value in
  * @value: the value to insert into the tree
  * Return: A pointer to the newly created node.
  */
-
 bst_t *bst_insert(bst_t **tree, int value)
 {
-	bst_t *tmp = NULL;
-	bst_t *second = NULL;
-	bst_t *new = NULL;
+bst_t *tmp = NULL;
+bst_t *second = NULL;
+bst_t *new = NULL;
 
 	if (!tree)
 		return (NULL);
@@ -48,4 +48,3 @@ bst_t *bst_insert(bst_t **tree, int value)
 
 	return (new);
 }
-
